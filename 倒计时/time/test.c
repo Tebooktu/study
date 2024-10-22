e6da20427f46b1f4fcd648855d4b1802f208747a
绅士兔 <2873452928@qq.com>
2024-10-22 08:45:22 +0800

    Add files via upload

---
+++ 倒计时/time/test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+int main()
+{
+	int a;
+		a = 60;
+		system("color 0a");
+		while (a >= 0)
+		{
+			system("cls");
+			printf("%d", a);
+			Sleep(1000);
+			a--;
+		}
+	return 0;
+}
